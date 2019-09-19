570138bd96e62e9811d28a281119e1f5b2ed60c2
Sergey Boytsov <sergey.boytsov@jetbrains.com>
2019-09-19 18:13:53 +0300

    third

--- matcher.py
+++ matcher.py
@@ -3,7 +3,7 @@ import itertools
 import json
 import os
 
-
+from matching.games import HospitalResident
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -55,27 +55,27 @@ def matcher_handler(event, context):
         "result": "FAIL"
     }
 
+    # gathering already matched pairs
+    history = []
+    locations = set()
+    langs = set()
+
+    for user, userdata in userdata_this_time.items():
+        history_pairs = [(user, i) for i in userdata['history']]
+        history.extend(history_pairs)
+        locations.add(userdata['location'])
+        langs.update(userdata['lang'])
+    clear_history = list({frozenset(c) for c in history})
+
     #match
     if algorithm == 'random':
-
-        #gathering already matched pairs
-        history = []
-        locations = set()
-        langs = set()
-
-        for user, userdata in userdata_this_time.items():
-            history_pairs = [ (user,i) for i in userdata['history'] ]
-            history.extend(history_pairs)
-            locations.add(userdata['location'])
-            langs.update(userdata['lang'])
-        clear_history = list({frozenset(c) for c in history})
 
         matching = []
         unmatched_users_by_location_and_lang_for_remote = []
 
-        #first - remote | lang
-        #second - local | lang
-        for lang,location  in list(itertools.product(langs, locations)):
+        #first - local | lang
+        #second - remote | lang
+        for lang,location in list(itertools.product(langs, locations)):
             #get suitable users for that lang and location
             users = get_users_for_lang_and_location(lang, location, userdata_this_time)
             #get all combinations for that users with respect of history
@@ -113,7 +113,47 @@ def matcher_handler(event, context):
             "result": "OK",
             "matched": matching
         }
+
     elif algorithm == 'marriage':
+
+        matching = []
+        unmatched_users_by_location_and_lang_for_remote = []
+
+        for lang,location in list(itertools.product(langs, locations)):
+            #get suitable users for that lang and location
+            users = get_users_for_lang_and_location(lang, location, userdata_this_time)
+            pairs_by_interests = combinate_users_with_respect_interests_and_history(users, clear_history, userdata_this_time)
+
+            matched_users = set()
+            for item in pairs_by_interests:
+                if item[0] in matched_users or item[1] in matched_users:
+                    pass
+                else:
+                    matching.append(item)
+                    matched_users.add(item[0])
+                    matched_users.add(item[1])
+
+            #need this for remote_coffee
+            unmatched_users = set(users).difference(set(matched_users))
+            unmatched_users_by_location_and_lang_for_remote.extend(list(unmatched_users))
+
+            remote_users = []
+            for user in set(unmatched_users_by_location_and_lang_for_remote):
+                if userdata_this_time[user]['remote']:
+                    remote_users.append(user)
+
+            remote_combinations = combinate_users_with_respect_interests_and_history(remote_users, clear_history, userdata_this_time)
+            matched_users = set()
+            for item in remote_combinations:
+                if item[0] in matched_users or item[1] in matched_users:
+                    pass
+                else:
+                    matching.append(item)
+                    matched_users.add(item[0])
+                    matched_users.add(item[1])
+
+            print(matching)
+
 
         result = {
             "result": "OK"
@@ -121,4 +161,20 @@ def matcher_handler(event, context):
 
     logging.info("@@##==================NEW MATCHES=======================================")
     logging.info(json.dumps(result))
-    return json.dumps(result)
+    return result
+
+
+def combinate_users_with_respect_interests_and_history(users, clear_history, userdata_this_time):
+    user_prefs = {}
+    user_for_interests = {}
+    for user in users:
+        user_prefs[user] = userdata_this_time[user]['interests']
+        for interest in userdata_this_time[user]['interests']:
+            if interest not in user_for_interests.keys():
+                user_for_interests[interest] = []
+            user_for_interests[interest].append(user)
+    capacities = {hosp: 2 for hosp in user_for_interests}
+    game = HospitalResident.create_from_dictionaries(user_prefs, user_for_interests, capacities)
+    pairs_by_interests = [tuple(x) for x in game.solve().values() if len(tuple(x)) == 2]
+    combinations_without_history = set(pairs_by_interests) - set(clear_history)
+    return combinations_without_history
--- test_matcher_handler.py
+++ test_matcher_handler.py
@@ -6,9 +6,10 @@ class test_matcher_handler(TestCase):
     def test_matcher_handler(self):
 
         matcher_handler({
-            "user_0": {"history": ["user_1"], "interests": [], "remote": True, "lang": ["ru"],"location": "space"},
-            "user_1": {"history":["user_0"],"interests":[],"remote":True,"lang": ["ru"], "location": "space"},
-            "user_2": {"history": [], "interests": [], "remote": True, "lang": ["ru"], "location": "space"}
+            "user_0": {"history": [], "interests": ["a","b"], "remote": True, "lang": ["ru"],"location": "space"},
+            "user_1": {"history":[],"interests":["b","c"],"remote":True,"lang": ["ru"], "location": "space"},
+            "user_2": {"history": [], "interests": ["c"], "remote": True, "lang": ["ru"], "location": "space"},
+            "user_3": {"history": [], "interests": ["a","d"], "remote": True, "lang": ["ru"], "location": "space"}
         },None)
 
         return
